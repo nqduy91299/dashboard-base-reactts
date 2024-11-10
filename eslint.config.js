fbd36050b90e30312b925a836a9fb3376c2a82a9
nqduy91299 <nguyenquocduy91299@gmail.com>
2024-11-10 18:02:48 +0700

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from '@eslint/js';
+import prettierPluginSortImports from '@trivago/prettier-plugin-sort-imports';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    { ignores: ['dist'] },
+    {
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+            eslintConfigPrettier,
+        ],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            '@trivago/prettier-plugin-sort-imports': prettierPluginSortImports,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            'sort-imports': [
+                'error',
+                {
+                    ignoreCase: false,
+                    ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+                    ignoreMemberSort: false,
+                    memberSyntaxSortOrder: [
+                        'none',
+                        'all',
+                        'multiple',
+                        'single',
+                    ],
+                    allowSeparatedGroups: true,
+                },
+            ],
+        },
+    },
+);
